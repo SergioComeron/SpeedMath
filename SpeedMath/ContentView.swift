d256c9d860604dc149f7a816c2f53c26d2f713cc
Sergio Comerón <sergiocomeron@icloud.com>
2020-04-05 12:55:48 +0200

    Initial Commit

---
+++ SpeedMath/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SpeedMath
+//
+//  Created by Sergio Comerón Sánchez-Pani on 05/04/2020.
+//  Copyright © 2020 Sergio Comerón Sánchez-Pani. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
